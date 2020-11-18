95b190d7240a31b0bba4b0f346c44ef6ae84e6ed
Nadir Nadjem <nadjemnadir47@gmail.com>
2020-11-18 19:16:58 +0100

    Update var.tf

--- var.tf
+++ var.tf
@@ -5,3 +5,47 @@ variable "ibm_region" {
   type        = string
   default     = "eu-de"
 }
+variable "log_mon_ns" {
+  type        = string
+  default     = "namespest"
+}
+
+
+variable provision_activity_tracker {
+    type        = bool
+    default     = false
+}
+
+variable activity_tracker_name {
+    type        = string
+    default     = "activity-tracker"
+}
+
+
+variable bring_your_own_logging {
+  type        = bool
+  default     = false
+}
+
+
+variable logging_instance_name {
+    type        = string
+   # default     = "logDna-cluster"
+}
+
+variable logging_agent_image {
+    description = "ICR image link for logdna agent"
+    type        = string
+    default     = "icr.io/ext/logdna-agent:latest"
+}
+
+variable logging_endpoint {
+    description = "API endpoint prefix for LogDNA (private, public)"
+    type        = string
+    default     = "private"
+}
+
+variable logging_plan {
+    type        = string
+    default     = "lite"
+}
