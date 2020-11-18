336b6b7305341f4e69f1d097ef5afbde6c8b43c9
Nadir Nadjem <nadjemnadir47@gmail.com>
2020-11-18 17:53:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,20 @@
 provider "ibm" {
   ibmcloud_api_key = "${var.ibm_api_key}"
 }
-
 data ibm_resource_group group {
   name = "ITGP_DATA"
 }
+# name of VPC
+
+resource "ibm_container_cluster" "cluster1_demo" {
+  name            = "cluster1_demo"
+  gateway_enabled = true 
+  datacenter      = "fra02"
+  machine_type    = "u2c.2x4"
+  hardware        = "shared"
+  default_pool_size = 1
+  worker_num=1
+  resource_group_id = data.ibm_resource_group.group.id
+}
+
+
