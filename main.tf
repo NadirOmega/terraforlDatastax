023d76e6dc6805370465cf4ef858566e8c130349
Nadir Nadjem <nadjemnadir47@gmail.com>
2020-11-18 23:19:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -85,3 +85,62 @@ module logging {
     end_points         = "public"
 }
 
+#Sysdig Part
+#Config
+  ##############################################################################
+# Locally define image pull secrets to copy to ibm-observe namespace 
+##############################################################################
+
+locals {
+  image_pull_secrets = [
+      "all-icr-io"
+    ]
+} 
+
+##############################################################################
+
+
+##############################################################################
+# Create Namespace
+##############################################################################
+
+resource kubernetes_namespace ibm_observe {
+  metadata {
+    name = "ibm-observe"
+  }
+}
+
+##############################################################################
+
+
+##############################################################################
+# Default pull secret
+##############################################################################
+
+data kubernetes_secret image_pull_secret {
+  count = length(local.image_pull_secrets)
+  metadata {
+    name = element(local.image_pull_secrets, count.index)
+  }
+}
+
+##############################################################################
+
+
+##############################################################################
+# Copy image pull secret to ibm-observe
+##############################################################################
+
+resource kubernetes_secret copy_image_pull_secret {
+  count = length(local.image_pull_secrets)
+  metadata {
+    name      = "ibm-observe-${element(local.image_pull_secrets, count.index)}"
+    namespace = kubernetes_namespace.ibm_observe.metadata.0.name
+  }
+  data      = {
+    ".dockerconfigjson" = data.kubernetes_secret.image_pull_secret[count.index].data[".dockerconfigjson"]
+  }
+  type = "kubernetes.io/dockerconfigjson"
+}
+
+##############################################################################
